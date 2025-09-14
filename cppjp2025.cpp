ea2664370f4bc38b1ae85abfda1cdddbcda1cd2b
lostjeta <lostjeta@naver.com>
2025-09-14 12:50:07 +0900

    첫 프로잭트 생성

---
+++ cppjp2025.cpp
@@ -0,0 +1,17 @@
+﻿// cppjp2025.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include "ZVector.h"
+
+int main()
+{
+    ZVector3 v1(1, 1, 1);
+    std::cout << v1 << std::endl;
+
+    ZVector3 v2(-1, 1, 1); // 외적
+    std::cout << v2.cross(v1) << std::endl;
+
+    std::cout << v1.dot(v2) << std::endl;
+
+}
